426b0fd74b10c71e475f6b725d2a997cf4b1dc74
dillon <1304937@qq.com>
2023-07-22 14:43:40 +0800

    IntervalUtils添加注释

--- androidutils/src/main/java/xyz/dcln/androidutils/utils/IntervalUtils.kt
+++ androidutils/src/main/java/xyz/dcln/androidutils/utils/IntervalUtils.kt
@@ -21,7 +21,18 @@ import java.util.concurrent.TimeUnit
 
 object IntervalUtils {
 
-    //获取IntervalController，不启动
+    /**
+     * 创建IntervalController对象，但不启动定时器。
+     *
+     * @param end 结束计时的时间点（以单位为unit的时间长度表示）。-1代表永不停止。end比start大,正计时，反之倒计时。
+     * @param period 定时器周期的长度（以单位为unit的时间长度表示）。
+     * @param unit 时间单位，如 TimeUnit.MILLISECONDS、TimeUnit.SECONDS 等。
+     * @param start 起始计时的时间点（以单位为unit的时间长度表示），默认为0。
+     * @param initialDelay 初始延迟时间（以单位为unit的时间长度表示），默认为0。
+     * @param onTick 每个周期触发的回调函数，接受一个 IntervalController 对象和当前计时值作为参数，默认为null。
+     * @param onFinish 计时结束时触发的回调函数，接受最终计时值作为参数，默认为null。
+     * @return 创建的 IntervalController 对象。
+     */
     fun create(
         end: Long,
         period: Long,
@@ -34,7 +45,18 @@ object IntervalUtils {
         return IntervalController(end, period, unit, start, initialDelay, onTick, onFinish)
     }
 
-    //直接启动interval
+    /**
+     * 直接启动定时器。
+     *
+     * @param end 结束计时的时间点（以单位为unit的时间长度表示）。-1代表永不停止。end比start大,正计时，反之倒计时。
+     * @param period 定时器周期的长度（以单位为unit的时间长度表示）。
+     * @param unit 时间单位，如 TimeUnit.MILLISECONDS、TimeUnit.SECONDS 等。
+     * @param start 起始计时的时间点（以单位为unit的时间长度表示），默认为0。
+     * @param initialDelay 初始延迟时间（以单位为unit的时间长度表示），默认为0。
+     * @param onTick 每个周期触发的回调函数，接受一个 IntervalController 对象和当前计时值作为参数，默认为null。
+     * @param onFinish 计时结束时触发的回调函数，接受最终计时值作为参数，默认为null。
+     * @param lifecycleOwner 用于绑定生命周期的 LifecycleOwner 对象，默认为null。
+     */
     fun interval(
         end: Long,
         period: Long,
@@ -50,6 +72,17 @@ object IntervalUtils {
         interval.start()
     }
 
+    /**
+     * 定时器控制器的内部类。
+     *
+     * @property end 结束计时的时间点（以单位为unit的时间长度表示）。
+     * @property period 定时器周期的长度（以单位为unit的时间长度表示）。
+     * @property unit 时间单位，如 TimeUnit.MILLISECONDS、TimeUnit.SECONDS 等。
+     * @property start 起始计时的时间点（以单位为unit的时间长度表示）。
+     * @property initialDelay 初始延迟时间（以单位为unit的时间长度表示）。
+     * @property onTick 每个周期触发的回调函数，接受一个 IntervalController 对象和当前计时值作为参数，默认为null。
+     * @property onFinish 计时结束时触发的回调函数，接受最终计时值作为参数，默认为null。
+     */
     class IntervalController(
         val end: Long,
         private val period: Long,
@@ -68,6 +101,11 @@ object IntervalUtils {
         var state = IntervalStatus.STATE_IDLE
             private set
 
+        /**
+         * 启动定时器。
+         *
+         * @return 当前 IntervalController 对象。
+         */
         fun start() = apply {
             if (state == IntervalStatus.STATE_ACTIVE) return this
             state = IntervalStatus.STATE_ACTIVE
@@ -75,6 +113,9 @@ object IntervalUtils {
             launch()
         }
 
+        /**
+         * 停止定时器。
+         */
         fun stop() {
             if (state == IntervalStatus.STATE_IDLE) return
             scope?.cancel()
@@ -82,14 +123,23 @@ object IntervalUtils {
             onFinish?.invoke(count)
         }
 
+        /**
+         * 取消定时器。
+         */
         fun cancel() {
             if (state == IntervalStatus.STATE_IDLE) return
             scope?.cancel()
             state = IntervalStatus.STATE_IDLE
         }
 
+        /**
+         * 关闭定时器。
+         */
         override fun close() = cancel()
 
+        /**
+         * 暂停定时器。
+         */
         fun pause() {
             if (state != IntervalStatus.STATE_ACTIVE) return
             scope?.cancel()
@@ -97,12 +147,18 @@ object IntervalUtils {
             delay = System.currentTimeMillis() - countTime
         }
 
+        /**
+         * 恢复定时器。
+         */
         fun resume() {
             if (state != IntervalStatus.STATE_PAUSE) return
             state = IntervalStatus.STATE_ACTIVE
             launch(delay)
         }
 
+        /**
+         * 重置定时器。
+         */
         fun reset() {
             count = start
             delay = unit.toMillis(initialDelay)
@@ -110,11 +166,20 @@ object IntervalUtils {
             if (state == IntervalStatus.STATE_ACTIVE) launch()
         }
 
+        /**
+         * 提前结束定时器。
+         */
         fun finishEarly() {
             cancel()
             onFinish?.invoke(count)
         }
 
+        /**
+         * 将定时器绑定到生命周期。
+         *
+         * @param lifecycleOwner 绑定生命周期的 LifecycleOwner 对象。
+         * @return 当前 IntervalController 对象。
+         */
         fun life(lifecycleOwner: LifecycleOwner) = apply {
             val lifecycle: Lifecycle = when (lifecycleOwner) {
                 is Fragment -> lifecycleOwner.viewLifecycleOwner.lifecycle
@@ -132,6 +197,12 @@ object IntervalUtils {
             })
         }
 
+        /**
+         * 将定时器绑定到生命周期。
+         *
+         * @param lifecycle 绑定生命周期的 Lifecycle 对象。
+         * @return 当前 IntervalController 对象。
+         */
         fun life(lifecycle: Lifecycle) = apply {
             lifecycle.addObserver(object : LifecycleEventObserver {
                 override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
@@ -162,11 +233,25 @@ object IntervalUtils {
             }
         }
 
+        /**
+         * 定时器的状态枚举类。
+         */
         enum class IntervalStatus {
             STATE_ACTIVE, STATE_IDLE, STATE_PAUSE
         }
     }
 
+    /**
+     * 在 LifecycleOwner 上启动定时器。
+     *
+     * @param end 结束计时的时间点（以单位为unit的时间长度表示）。-1代表永不停止。end比start大,正计时，反之倒计时。
+     * @param period 定时器周期的长度（以单位为unit的时间长度表示）。
+     * @param unit 时间单位，如 TimeUnit.MILLISECONDS、TimeUnit.SECONDS 等。
+     * @param start 起始计时的时间点（以单位为unit的时间长度表示），默认为0。
+     * @param initialDelay 初始延迟时间（以单位为unit的时间长度表示），默认为0。
+     * @param onTick 每个周期触发的回调函数，接受一个 IntervalController 对象和当前计时值作为参数，默认为null。
+     * @param onFinish 计时结束时触发的回调函数，接受最终计时值作为参数，默认为null。
+     */
     fun LifecycleOwner.interval(
         end: Long,
         period: Long,
