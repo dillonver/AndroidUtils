89f52c6daf6a9f04c74c1430a024e223ac2b69c3
hby-dcl <1304937@qq.com>
2023-09-26 15:06:24 +0800

    update ImageUtils

--- androidutils/src/main/java/xyz/dcln/androidutils/utils/ImageUtils.kt
+++ androidutils/src/main/java/xyz/dcln/androidutils/utils/ImageUtils.kt
@@ -1,8 +1,13 @@
 package xyz.dcln.androidutils.utils
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.drawable.Drawable
+import android.util.Base64
 import androidx.annotation.DrawableRes
 import androidx.core.content.ContextCompat
+import java.io.ByteArrayOutputStream
+import java.io.File
 
 /**
  * Created by dcl on 2023/7/7.
@@ -10,5 +15,28 @@ import androidx.core.content.ContextCompat
 object ImageUtils {
     fun getDrawable(@DrawableRes id: Int): Drawable? {
         return ContextCompat.getDrawable(AppUtils.getApp(), id)
+    }
+
+    fun encodeImageToBase64(imageFile: File): String? {
+        return if (imageFile.exists()) {
+            val bitmap = BitmapFactory.decodeFile(imageFile.absolutePath)
+            encodeBitmapToBase64(bitmap)
+        } else {
+            null
+        }
+    }
+
+    fun encodeImageToBase64(imagePath: String): String? {
+        return encodeImageToBase64(File(imagePath))
+    }
+
+    private fun encodeBitmapToBase64(bitmap: Bitmap): String {
+        // Convert the bitmap to byte array
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream)
+        val byteArray: ByteArray = byteArrayOutputStream.toByteArray()
+
+        // Encode byte array to Base64 string
+        return Base64.encodeToString(byteArray, Base64.DEFAULT)
     }
 }
