ffd1a245ae6e934b9f7b491ccba4d8ecf3cfe00d
dillon <1304937@qq.com>
2024-05-22 12:21:34 +0800

    update and fix

--- androidutils/src/main/java/xyz/dcln/androidutils/utils/IntentUtils.kt
+++ androidutils/src/main/java/xyz/dcln/androidutils/utils/IntentUtils.kt
@@ -16,37 +16,43 @@ object IntentUtils {
      * @return 返回对应键的值
      */
     inline fun <reified T> getValueFromIntent(intent: Intent, key: String): T? {
-        @Suppress("UNCHECKED_CAST")
-        return when (T::class) {
-            Int::class -> intent.getIntExtra(key, 0) as T?
-            Float::class -> intent.getFloatExtra(key, 0f) as T?
-            Double::class -> intent.getDoubleExtra(key, 0.0) as T?
-            Long::class -> intent.getLongExtra(key, 0L) as T?
-            String::class -> intent.getStringExtra(key) as T?
-            Boolean::class -> intent.getBooleanExtra(key, false) as T?
-            Bundle::class -> intent.getBundleExtra(key) as T?
-            Parcelable::class -> {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                    intent.getParcelableExtra(key, T::class.java) as T?
-                } else {
-                    @Suppress("DEPRECATION")
-                    intent.getParcelableExtra(key) as T?
+        return try {
+            @Suppress("UNCHECKED_CAST")
+            when (T::class) {
+                Int::class -> intent.getIntExtra(key, 0) as T?
+                Float::class -> intent.getFloatExtra(key, 0f) as T?
+                Double::class -> intent.getDoubleExtra(key, 0.0) as T?
+                Long::class -> intent.getLongExtra(key, 0L) as T?
+                String::class -> intent.getStringExtra(key) as T?
+                Boolean::class -> intent.getBooleanExtra(key, false) as T?
+                Bundle::class -> intent.getBundleExtra(key) as T?
+                Parcelable::class -> {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                        intent.getParcelableExtra(key, T::class.java) as T?
+                    } else {
+                        @Suppress("DEPRECATION")
+                        intent.getParcelableExtra(key) as T?
+                    }
+                }
+
+                Serializable::class -> {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                        intent.getSerializableExtra(key, (T::class.java) as Class<Serializable>) as T?
+                    } else {
+                        @Suppress("DEPRECATION")
+                        intent.getSerializableExtra(key) as? T
+                    }
+                }
+
+                else -> {
+                    val jsonString = intent.getStringExtra(key)
+                    jsonString?.let { GsonUtils.fromJson(it, T::class.java) }
                 }
             }
-
-            Serializable::class -> {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                    intent.getSerializableExtra(key, (T::class.java) as Class<Serializable>) as T?
-                } else {
-                    @Suppress("DEPRECATION")
-                    intent.getSerializableExtra(key) as? T
-                }
-            }
-
-            else -> {
-                val jsonString = intent.getStringExtra(key)
-                jsonString?.let { GsonUtils.fromJson(it, T::class.java) }
-            }
+        } catch (e: Exception) {
+            e.printStackTrace() // 可以根据需要将异常信息记录到日志中
+            null // 在发生异常时返回 null
         }
     }
+
 }
